645a76bbb449bc240c6e8ab083add2497f202d7f
Abdelmalek Eladjelet <128822399+Eladjeletabdelmalek@users.noreply.github.com>
2024-11-11 23:54:07 +0100

    tp2.pl

--- tp2.pl
+++ tp2.pl
@@ -3,9 +3,12 @@ belongs(X,[_,L]):-belongs(X,L).
 %Q2
 belongs(X,[X|L]):-write("yes it is").
 %Q3
-last_element(X,[L,X]):- write("yes it is").
+last_element(X,[X]).
+last_element(X,[Y|L]):-last_element(X,L),write("yes it is").
 %Q4
-is_panultimte(X,[_|[X,L]]):- write("yes it is").
+is_penultimate(X, [X, _]) :- !.
+is_penultimate(X, [_ | T]) :-
+    is_penultimate(X, T).
 %Q5
 remove_the_kth(1, [_ | Tail], Tail).
 remove_the_kth(K, [H | T], [H | R]) :-
@@ -16,6 +19,7 @@ remove_the_kth(K, [H | T], [H | R]) :-
 list_length([], 0).
 list_length([_ | L], N) :-list_length(L, N1),N is N1 + 1.
 %Q7
+even([]).
 even([X|L]):-X mod 2=:=0,even(L).
 %Q8
 concat_list([], L2, L2).
